9904c19e6f0f07c7aaf4de85296ae1d5168619af
Ben Langfeld <ben@langfeld.me>
2016-05-03 09:31:18 -0300

    De-lint

--- lib/puppet/reports/snmp.rb
+++ lib/puppet/reports/snmp.rb
@@ -12,11 +12,11 @@ end
 
 Puppet::Reports.register_report(:snmp) do
 
-  #connect to PuppetDB to get ip for the node
+  # connect to PuppetDB to get ip for the node
   PuppetDB::Connection.check_version
 
-  configfile = File.join([File.dirname(Puppet.settings[:config]), "snmp.yaml"])
-  raise(Puppet::ParseError, "SNMP report config file #{configfile} not readable") unless File.exist?(configfile)
+  configfile = File.join(File.dirname(Puppet.settings[:config]), "snmp.yaml")
+  raise Puppet::ParseError, "SNMP report config file #{configfile} not readable" unless File.exist?(configfile)
   config = YAML.load_file(configfile)
   SNMP_SERVER  = config[:snmp_server]
   SNMP_VERSION = config[:snmp_version]
@@ -27,7 +27,6 @@ Puppet::Reports.register_report(:snmp) do
 
   def process
     if self.status == 'failed'
-
       uri = URI(Puppet::Util::Puppetdb.config.server_urls.first)
       puppetdb = PuppetDB::Connection.new(uri.host, uri.port, uri.scheme == 'https')
       parser = PuppetDB::Parser.new
@@ -39,15 +38,15 @@ Puppet::Reports.register_report(:snmp) do
       msg = "Puppet run for #{self.host} #{self.status} at #{Time.now.asctime} on #{self.configuration_version} in #{self.environment}"
       hostname_bind = VarBind.new("1.3.18.0.2.4.486", OctetString.new(self.host))
       puppet_env_bind = VarBind.new("1.3.6.1.4.1.34380.1.1.12", OctetString.new(self.environment))
+      trap_options = [hostname_bind, puppet_env_bind]
+      
       if SNMP_VERSION =~ /v2/
-        trap_options = [hostname_bind, puppet_env_bind]
-        SNMP::Manager.open(:Host => "#{SNMP_SERVER}",:Version => :SNMPv2c) do |snmp|
-            snmp.trap_v2(12345,"1.3.6.1.4.1.34380",trap_options)
+        SNMP::Manager.open(:Host => SNMP_SERVER, :Version => :SNMPv2c) do |snmp|
+          snmp.trap_v2(12345, "1.3.6.1.4.1.34380", trap_options)
         end
       else
-        trap_options = [hostname_bind, puppet_env_bind]
-        SNMP::Manager.open(:Host => "#{SNMP_SERVER}",:Version => :SNMPv1) do |snmp|
-          snmp.trap_v1("enterprises.34380", "#{ip_address}", :enterpriseSpecific, 42, 12345, trap_options)
+        SNMP::Manager.open(:Host => SNMP_SERVER, :Version => :SNMPv1) do |snmp|
+          snmp.trap_v1("enterprises.34380", ip_address, :enterpriseSpecific, 42, 12345, trap_options)
         end
       end
     end
