5fc8bd826d508155e2981ee342e369ac7c6dbd85
Ben Langfeld <ben@langfeld.me>
2016-05-03 09:57:25 -0300

    Extract some magical values
    Give them names and explain where they come from

--- lib/puppet/reports/snmp.rb
+++ lib/puppet/reports/snmp.rb
@@ -20,6 +20,11 @@ Puppet::Reports.register_report(:snmp) do
   config = YAML.load_file(configfile)
   SNMP_SERVER  = config[:snmp_server]
   SNMP_VERSION = config[:snmp_version]
+  
+  PUPPETLABS_OID = "1.3.6.1.4.1.34380"
+  PUPPET_ENVIRONMENT_OID = "1.3.6.1.4.1.34380.1.1.12"
+  HOSTNAME_OID = "1.3.18.0.2.4.486"
+  SPECIFIC_TRAP_ID = 42 # Randomly chosen to represent agent run failure
 
   desc <<-DESC
   Send notification of failed reports to an SNMP server.
@@ -29,13 +34,15 @@ Puppet::Reports.register_report(:snmp) do
     if self.status == 'failed'
       Puppet.debug "Sending status for #{self.host} to SNMP server #{SNMP_SERVER} at #{Time.now.asctime} due to run status #{self.status} on #{self.configuration_version} in #{self.environment}"
       
+      system_uptime = 12345 # Dummy value because we must provide one
+      
       if SNMP_VERSION =~ /v2/
         SNMP::Manager.open(:Host => SNMP_SERVER, :Version => :SNMPv2c) do |snmp|
-          snmp.trap_v2(12345, "1.3.6.1.4.1.34380", trap_options)
+          snmp.trap_v2(system_uptime, PUPPETLABS_OID, trap_options)
         end
       else
         SNMP::Manager.open(:Host => SNMP_SERVER, :Version => :SNMPv1) do |snmp|
-          snmp.trap_v1("enterprises.34380", ip_address, :enterpriseSpecific, 42, 12345, trap_options)
+          snmp.trap_v1(PUPPETLABS_OID, ip_address, :enterpriseSpecific, SPECIFIC_TRAP_ID, system_uptime, trap_options)
         end
       end
     end
@@ -54,8 +61,8 @@ private
   end
   
   def trap_options
-    hostname_bind = VarBind.new("1.3.18.0.2.4.486", OctetString.new(self.host))
-    puppet_env_bind = VarBind.new("1.3.6.1.4.1.34380.1.1.12", OctetString.new(self.environment))
+    hostname_bind = VarBind.new(HOSTNAME_OID, OctetString.new(self.host))
+    puppet_env_bind = VarBind.new(PUPPET_ENVIRONMENT_OID, OctetString.new(self.environment))
     [hostname_bind, puppet_env_bind]
   end
 end
